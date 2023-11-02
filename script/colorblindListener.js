2d4ecb2e072fead514873d613e50dd30f4b8a3e3
Thiago Cruz Machado <96290053+yTrosky@users.noreply.github.com>
2023-11-02 17:38:46 -0300

    Update colorblindListener.js

--- script/colorblindListener.js
+++ script/colorblindListener.js
@@ -64,7 +64,10 @@ filtersObj.achromatopsia = () => {
   document.body.appendChild(filterID);
 
   filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="achromatopsia"> <feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter>  </defs> </svg>';
-  stylingID.innerHTML = 'html{-webkit-filter:url(#achromatopsia);-moz-filter:(#achromatopsia);-ms-filter:(#achromatopsia);-o-filter:(#achromatopsia);filter:(#achromatopsia);}'
+  
+
+  stylingID.innerHTML = 'html { filter: contrast(150%) brightness(120%); }';
+
   setTimeout(function () {
     window.scrollBy(1, 1);
     window.scrollBy(-1, -1);
@@ -86,7 +89,7 @@ filtersObj.deuteranopia = () => {
   filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
   document.body.appendChild(filterID);
 
-  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="deuteranopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.29031,0.70969,-0.00000,0,0 0.29031,0.70969,-0.00000,0,0 -0.02197,0.02197,1.00000,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="deuteranopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="1.2, 0, 0, 0, 0,0, 1, 0, 0, 0,0, 0, 0.8, 0, 0,0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
   stylingID.innerHTML = 'html{-webkit-filter:url(#deuteranopia);-moz-filter:(#deuteranopia);-ms-filter:(#deuteranopia);-o-filter:(#deuteranopia);filter:(#deuteranopia);}'
   setTimeout(function () {
     window.scrollBy(1, 1);
@@ -109,7 +112,7 @@ filtersObj.deuteranomaly = () => {
   filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
   document.body.appendChild(filterID);
 
-  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="deuteranomaly" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.57418,0.42582,-0.00000,0,0 0.17418,0.82582,-0.00000,0,0 -0.01318,0.01318,1.00000,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="deuteranomaly" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="1, 0.7, 0, 0, 0,0, 1, 0, 0, 0,0, 0, 1, 0, 0,0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
   stylingID.innerHTML = 'html{-webkit-filter:url(#deuteranomaly);-moz-filter:(#deuteranomaly);-ms-filter:(#deuteranomaly);-o-filter:(#deuteranomaly);filter:(#deuteranomaly);}'
   setTimeout(function () {
     window.scrollBy(1, 1);
@@ -132,7 +135,7 @@ filtersObj.protanomaly = () => {
   filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
   document.body.appendChild(filterID);
 
-  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanomaly" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.46533,0.53467,-0.00000,0,0 0.06533,0.93467,0.00000,0,0 0.00268,-0.00268,1.00000,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanomaly" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="1, 0.5, 0, 0, 0,0, 1, 0, 0, 0,0, 0, 1, 0, 0,0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
   stylingID.innerHTML = 'html{-webkit-filter:url(#protanomaly);-moz-filter:(#protanomaly);-ms-filter:(#protanomaly);-o-filter:(#protanomaly);filter:(#protanomaly);}'
   setTimeout(function () {
     window.scrollBy(1, 1);
@@ -155,7 +158,7 @@ filtersObj.protanopia = () => {
   filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
   document.body.appendChild(filterID);
 
-  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.10889,0.89111,-0.00000,0,0 0.10889,0.89111,0.00000,0,0 0.00447,-0.00447,1.00000,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="protanopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="1, 0, 0, 0, 0,0.5, 1, 0, 0, 0,0, 0, 1, 0, 0,0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
   stylingID.innerHTML = 'html{-webkit-filter:url(#protanopia);-moz-filter:(#protanopia);-ms-filter:(#protanopia);-o-filter:(#protanopia);filter:(#protanopia);}'
   setTimeout(function () {
     window.scrollBy(1, 1);
@@ -178,7 +181,7 @@ filtersObj.tritanomaly = () => {
   filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
   document.body.appendChild(filterID);
 
-  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="tritanomaly" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="1.00000,0.09142,-0.09142,0,0 0.00000,0.92030,0.07970,0,0 -0.00000,0.52030,0.47970,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="tritanomaly" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.75, 0, 0, 0, 0,0, 1, 0, 0, 0,0, 0, 1.25, 0, 0,0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
   stylingID.innerHTML = 'html{-webkit-filter:url(#tritanomaly);-moz-filter:(#tritanomaly);-ms-filter:(#tritanomaly);-o-filter:(#tritanomaly);filter:(#tritanomaly);}'
   setTimeout(function () {
     window.scrollBy(1, 1);
@@ -201,7 +204,7 @@ filtersObj.tritanopia = () => {
   filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
   document.body.appendChild(filterID);
 
-  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="tritanopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="1.00000,0.15236,-0.15236,0,0 0.00000,0.86717,0.13283,0,0 -0.00000,0.86717,0.13283,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+  filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="tritanopia" color-interpolation-filters="linearRGB"> <feColorMatrix type="matrix" values="0.5, 0, 0, 0, 0,0, 1, 0, 0, 0,0, 0, 1.5, 0, 0,0, 0, 0, 1, 0" in="SourceGraphic" /> </filter> </defs> </svg>';
   stylingID.innerHTML = 'html{-webkit-filter:url(#tritanopia);-moz-filter:(#tritanopia);-ms-filter:(#tritanopia);-o-filter:(#tritanopia);filter:(#tritanopia);}'
   setTimeout(function () {
     window.scrollBy(1, 1);
@@ -280,4 +283,3 @@ document.querySelectorAll(['[id^="radio"]']).forEach(radioButton => {
     applyFilter((window.selectedFilter = filter));
   });
 });
-
